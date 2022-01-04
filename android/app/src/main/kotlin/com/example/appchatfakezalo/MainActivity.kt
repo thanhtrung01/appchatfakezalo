6ad7b54bc6e2b572969b75e759056331ea975761
phong47047 <92257648+phong47047@users.noreply.github.com>
2022-01-04 10:54:04 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/appchatfakezalo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appchatfakezalo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Authenticate/Autheticate.dart
@@ -0,0 +1,17 @@
+import 'package:appchatfakezalo/Screens/HomeScreen.dart';
+import 'package:appchatfakezalo/Authenticate/LoginScree.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_auth.currentUser != null) {
+      return HomeScreen();
+    } else {
+      return LoginScreen();
+    }
+  }
+}
